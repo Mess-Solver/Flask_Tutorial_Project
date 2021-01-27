d84fb84162bea2b92aecfc9d93378198fe9daa8e
Kamran Zaidi <kamranhimself@gmail.com>
2021-01-28 02:30:47 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from app import db
+
+
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(50),unique=True,index=True)
+    email=db.Column(db.String(50),unique=True, index=True)
+    password_hash=db.Column(db.String(100))
+
+    def __repr__(self):
+        return "User : {}".format(self.username)
+
